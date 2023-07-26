1c1937a82eb793ddfe823c04558e8ec34b59bfc4
DominikLis97 <113893785+DominikLis97@users.noreply.github.com>
2023-07-26 16:32:44 +0200

    feature: link payments logic implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-export const handleOpen = () => {
-  window.parent.postMessage({ type: EVENT_MESSAGES.open }, '*')
+export const handleOpen = (event = 'linkConnect') => {
+  if (!Object.keys(EVENT_MESSAGES).includes(event)) {
+    event = 'linkConnect'
+  }
+  window.parent.postMessage({ type: EVENT_MESSAGES[event] }, '*')
 }
 
 export const handleClose = (url) => {
@@ -7,19 +10,23 @@ export const handleClose = (url) => {
 }
 
 const EVENT_MESSAGES = {
-  open: 'blockmate-iframe-open',
-  close: 'blockmate-iframe-close'
+  linkConnect: 'https://link.blockmate.io/',
+  close: 'blockmate-iframe-close',
+  verifyPhone: 'https://link.blockmate.io/verify-phone',
+  changePhone: 'https://link.blockmate.io/change-phone',
+  enableTransfer: 'https://link.blockmate.io/enable-transfer',
+  transferAssets: 'https://link.blockmate.io/transfer-assets'
 }
 
-export const LinkModal = ({ jwt, url = 'https://link-dev.blockmate.io' }) => {
+export const LinkModal = (jwt) => {
   if (!jwt) return null
 
   const body = document.querySelector('body')
 
   const iframeStyle =
-    'display:block; position:fixed; width:500px; height:500px; z-index:100; border:none; top:0; right:0'
+    'display:block; position:fixed; width:100%; height:100%; z-index:100; border:none; top:0%; right:0'
 
-  const createIframe = () => {
+  const createIframe = (url) => {
     const iframe = document.createElement('iframe')
     iframe.setAttribute('src', `${url}/?jwt=${jwt}`)
     iframe.setAttribute('style', iframeStyle)
@@ -39,8 +46,20 @@ export const LinkModal = ({ jwt, url = 'https://link-dev.blockmate.io' }) => {
     if (event?.data?.type === EVENT_MESSAGES.close) {
       removeIframe(event)
     }
-    if (event?.data?.type === EVENT_MESSAGES.open) {
-      createIframe()
+    if (event?.data?.type === EVENT_MESSAGES.linkConnect) {
+      createIframe(EVENT_MESSAGES.linkConnect)
+    }
+    if (event?.data?.type === EVENT_MESSAGES.verifyPhone) {
+      createIframe(EVENT_MESSAGES.verifyPhone)
+    }
+    if (event?.data?.type === EVENT_MESSAGES.changePhone) {
+      createIframe(EVENT_MESSAGES.changePhone)
+    }
+    if (event?.data?.type === EVENT_MESSAGES.enableTransfer) {
+      createIframe(EVENT_MESSAGES.enableTransfer)
+    }
+    if (event?.data?.type === EVENT_MESSAGES.transferAssets) {
+      createIframe(EVENT_MESSAGES.transferAssets)
     }
   }
 
