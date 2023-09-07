6e8b2a494e85a747ceb5c90ad24c8960c0614049
Daniel Oravec <danoravec@gmail.com>
2023-09-07 10:59:07 +0200

    Add option to have cleanup actions run on close
    * Useful for example for automatic site
      refresh on modal close.
    
    * An object of cleanupActions is passed
      to the LinkModal. This object can contain
      arbitrary number of key-function pairs.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const EVENT_MESSAGES = {
   close: 'blockmate-iframe-close'
 }
 
-export const LinkModal = ({ jwt, url = 'https://link-dev.blockmate.io' }) => {
+export const LinkModal = ({ jwt, url = 'https://link-dev.blockmate.io', cleanupActions = {} }) => {
   if (!jwt) return null
 
   const body = document.querySelector('body')
@@ -20,11 +20,15 @@ export const LinkModal = ({ jwt, url = 'https://link-dev.blockmate.io' }) => {
     'display:block; position:fixed; width:100%; height:100%; z-index:100; border:none; top:0; right:0'
 
   const createIframe = () => {
-    const iframe = document.createElement('iframe')
-    iframe.setAttribute('src', `${url}/?jwt=${jwt}`)
-    iframe.setAttribute('style', iframeStyle)
-    iframe.setAttribute('id', 'link-iframe')
-    body.appendChild(iframe)
+    const iframeId = 'link-iframe';
+    const existingIframe = document.getElementById(iframeId);
+    if (!existingIframe) {
+      const iframe = document.createElement('iframe');
+      iframe.setAttribute('src', `${url}/?jwt=${jwt}`)
+      iframe.setAttribute('style', iframeStyle)
+      iframe.setAttribute('id', iframeId)
+      body.appendChild(iframe)
+    }
   }
 
   const removeIframe = (event) => {
@@ -32,6 +36,10 @@ export const LinkModal = ({ jwt, url = 'https://link-dev.blockmate.io' }) => {
     body.removeChild(iframe)
     if (event.data.url) {
       window.location = event.data.url
+    }
+    const endResult = event?.data?.endResult;
+    if (endResult && cleanupActions[endResult]) {
+      cleanupActions[endResult]();
     }
   }
 
