1a861ceebda1314292544dfbcfa25696dbca3b23
DominikLis97 <113893785+DominikLis97@users.noreply.github.com>
2023-12-21 23:08:28 +0100

    feature: link lib handle withdraw part v2.0.4

--- src/index.js
+++ src/index.js
@@ -21,7 +21,12 @@ const EVENT_MESSAGES = {
   cryptoSavings: `${linkUrl}crypto-savings`
 }
 
-export const LinkModal = ({ jwt, url, cleanupActions = {} }) => {
+export const LinkModal = ({
+  jwt,
+  url,
+  cleanupActions = {},
+  additionalUrlParams = null
+}) => {
   if (!jwt) return null
 
   const body = document.querySelector('body')
@@ -34,9 +39,21 @@ export const LinkModal = ({ jwt, url, cleanupActions = {} }) => {
     const existingIframe = document.getElementById(iframeId)
     if (!existingIframe) {
       const iframe = document.createElement('iframe')
-      iframe.setAttribute('src', `${url}?jwt=${jwt}&accountId=${accountId}`)
+      let additionalParamsStr = ''
+      if (additionalUrlParams) {
+        additionalParamsStr = Object.keys(additionalUrlParams)
+          .map(
+            (key) =>
+              `&${key}=${additionalUrlParams[key]}
+        `
+          )
+          .join('')
+      }
+      const urlWithParams = `${url}/?jwt=${jwt}&accountId=${accountId}${additionalParamsStr}`
+      iframe.setAttribute('src', urlWithParams)
       iframe.setAttribute('style', iframeStyle)
       iframe.setAttribute('id', iframeId)
+      iframe.setAttribute('allow', 'camera')
       body.appendChild(iframe)
     }
   }
